1ceaadf757d4a5eef8efa25b205f97715e1e20ac
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-08-26 10:07:04 -0500

    preloading image and fixed border radius of image

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 export function QRCode() {
   return (
     <div className="max-w-[320px] bg-white rounded-[20px] space-y-6 p-4 pb-10">
-      <div className="rounded-[20px] object-cover overflow-hidden">
+      <div className="rounded-[10px] object-cover overflow-hidden">
         <Image
           src="/image-qr-code.png"
           alt="QR Code"
           width={576}
           height={576}
+          priority
         />
       </div>
       <div className="space-y-4 text-center">
