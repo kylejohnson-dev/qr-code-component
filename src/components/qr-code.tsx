801900dfc046c04384751bc84eb410561d85b487
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-08-26 10:17:44 -0500

    updated letter spacing and added padding to the div containing text

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function QRCode() {
   return (
-    <div className="max-w-[320px] bg-white rounded-[20px] space-y-6 p-4 pb-10">
+    <div className="max-w-[320px] h-[499px] bg-white rounded-[20px] space-y-6 p-4 pb-10">
       <div className="rounded-[10px] object-cover overflow-hidden">
         <Image
           src="/image-qr-code.png"
@@ -12,11 +12,11 @@ export function QRCode() {
           priority
         />
       </div>
-      <div className="space-y-4 text-center">
-        <p className="text-slate-900 font-bold text-[22px] leading-[120%] tracking-0">
+      <div className="space-y-4 text-center px-4">
+        <p className="text-slate-900 font-bold text-[22px] leading-[120%] -tracking-[0.2px]">
           Improve your front-end skills by building projects
         </p>
-        <p className="text-slate-500 text-[15px] leading-[140%] tracking-[0.2px]">
+        <p className="text-slate-500 text-[15px] leading-[140%] -tracking-[0.2px]">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
