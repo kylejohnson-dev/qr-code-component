6ab5c215f21ccda2962ec61b5e06fc87caad059a
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-08-26 10:38:38 -0500

    removed padding from div element parent of main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { QRCode } from "@/components/qr-code";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-4">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <QRCode />
       </main>
