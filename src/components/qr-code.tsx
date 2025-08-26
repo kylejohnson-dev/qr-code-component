1139cf518539b725fd390854b0aed54787429803
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-08-26 10:52:00 -0500

    returned letting-spacing to design spec and added the correct font-family

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function QRCode() {
   return (
-    <section className="max-w-[320px] h-[499px] bg-white rounded-[20px] space-y-6 p-4 pb-10">
+    <div className="max-w-[320px] h-[499px] bg-white rounded-[20px] space-y-6 p-4 pb-10">
       <div className="rounded-[10px] object-cover overflow-hidden">
         <Image
           src="/image-qr-code.png"
@@ -13,14 +13,14 @@ export function QRCode() {
         />
       </div>
       <div className="space-y-4 text-center px-4">
-        <h1 className="text-slate-900 font-bold text-[22px] leading-[120%] -tracking-[0.2px]">
+        <h1 className="font-[family-name:var(--font-outfit)] text-slate-900 font-bold text-[22px] leading-[120%] tracking-[0px]">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-slate-500 text-[15px] leading-[140%] -tracking-[0.2px]">
+        <p className="text-slate-500 text-[15px] leading-[140%] tracking-[0.2px]">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
       </div>
-    </section>
+    </div>
   );
 }
